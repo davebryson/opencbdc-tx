3b3450bf2ea3e4ea35f9a2464099939b0eee20ac
Dave Bryson <daveb@miceda.org>
2022-05-01 08:31:18 -0500

    slight adjustments to scripts. return to script vs cmake for envvars. moved sudo lines needed
    Signed-off-by: Dave Bryson <daveb@miceda.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,14 @@ fi
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
+CMAKE_FLAGS=""
 CPUS=1
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     CPUS=$(grep -c ^processor /proc/cpuinfo)
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     CPUS=$(sysctl -n hw.ncpu)
+    XCODE_CMDLINE_DIR=$(xcode-select -p)
+    CMAKE_FLAGS+="-DCMAKE_C_COMPILER=${XCODE_CMDLINE_DIR}/usr/bin/clang -DCMAKE_CXX_COMPILER=${XCODE_CMDLINE_DIR}/usr/bin/clang++ -DCMAKE_CXX_FLAGS=-isystem\ /usr/local/include -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 fi
 
 CMAKE_BUILD_TYPE="Debug"
@@ -24,5 +27,5 @@ elif [[ "$BUILD_PROFILING" == "1" ]]; then
     CMAKE_BUILD_TYPE="Profiling"
 fi
 
-eval "cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .."
+eval "cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_FLAGS} .."
 make -j$CPUS
